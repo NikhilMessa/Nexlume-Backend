1b89796bf4262c093de857e2b94a85342ecd80f4
NikhilMessa20 <nikhil.m@linkcxo.com>
2026-06-18 15:47:16 +0530

    New Dev Server is Added

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const allowedOrigins = process.env.CORS_ORIGINS
       'https://nexlume.netlify.app',
       'http://localhost:3000',
       'http://127.0.0.1:5173',
+      'https://nexlume-dev.netlify.app',
     ];
 
 app.use(
